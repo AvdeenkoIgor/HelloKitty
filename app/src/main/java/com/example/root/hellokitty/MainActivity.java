e61ec3cd20a8ce915a95d87c284b815ceada283c
Igor Avdeenko <avdeenko@ukr.net>
2016-02-26 16:41:56 +0200

    First Lesson + Second Lesson

--- app/src/main/java/com/example/root/hellokitty/MainActivity.java
+++ app/src/main/java/com/example/root/hellokitty/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.root.hellokitty;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,13 +11,22 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mHelloTextView;
     private EditText mNameEditText;
+    private Button mCrowsCounterButton;
+    private int mCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mHelloTextView = (TextView)findViewById(R.id.textView);
+        mCrowsCounterButton = (Button)findViewById(R.id. ButtonCrowsCounter);
+        mHelloTextView = (TextView)findViewById(R.id.textViewHello);
         mNameEditText = (EditText) findViewById(R.id.editText);
+        mCrowsCounterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mHelloTextView.setText("Я насчитал " + ++mCount + " ворон");
+            }
+        });
     }
 
     public void onClick(View view) {
@@ -25,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         } else {
             mHelloTextView.setText("Привет, " + mNameEditText.getText());
         }
+    }
 
+    public void onClick2(View view) {
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        textView2.setText("Hello Kitty!");
     }
 }
+
